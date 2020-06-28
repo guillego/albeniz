8e8a09bc47194546aa9d13ac952bf34d384fbb0e
Guillermo <guillermo@radiobotics.com>
2020-06-28 20:06:41 +0200

    Cookiecutter structure

---
+++ albeniz/examples/quickstart.py
@@ -0,0 +1,16 @@
+"""
+This example script imports the albeniz package and
+prints out the version.
+"""
+
+import albeniz
+
+
+def main():
+    print(
+        f"albeniz version: {albeniz.__version__}"
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ albeniz/src/albeniz/__init__.py
@@ -0,0 +1,5 @@
+"""
+Spotify music analyzer
+"""
+
+__version__ = "0.0.1"
